961dad21ad033abfb690b72dd2970968c3a848ee
Dennis Kok <denniskok@fastmail.com>
2023-10-06 18:45:51 +0200

    Implement result filtering for Qwant

--- content-search-filtering.js
+++ content-search-filtering.js
@@ -294,6 +294,9 @@ function filterSearchResults(searchResults, searchEngine, storage) {
                   case 'ecosia':
                     document.querySelector('body').prepend(searchRemovalNotice);
                     break;
+                  case 'qwant':
+                    document.querySelector('div[data-testid=sectionWeb]').prepend(searchRemovalNotice);
+                    break;
                   case 'startpage':
                     document.querySelector('#main').prepend(searchRemovalNotice);
                     break;
@@ -341,6 +344,12 @@ function filterSearchResults(searchResults, searchEngine, storage) {
                   if (searchResult.closest('div.mainline__result-wrapper')) {
                     cssQuery = 'div.mainline__result-wrapper';
                     searchResultContainer = searchResult.closest(cssQuery);
+                  }
+                  break;
+                case 'qwant':
+                  if (searchResult.closest('div[data-testid=webResult]')) {
+                    cssQuery = 'div[data-testid=webResult]';
+                    searchResultContainer = searchResult.closest(cssQuery).parentElement;
                   }
                   break;
                 case 'startpage':
@@ -475,6 +484,23 @@ function main(mutations = null, observer = null) {
               document.addEventListener('readystatechange', e => {
                 if (['interactive', 'complete'].includes(document.readyState)) {
                   filterEcosia();
+                }
+              }, { once: true });
+            }
+          } else if (currentURL.hostname.includes('qwant.com')) {
+            // Function to filter search results in Qwant
+            function filterQwant() {
+              let searchResults = Array.from(document.querySelectorAll('a[data-testid=serTitle]')).filter(el => el.href.includes('fandom.com') || el.href.includes('fextralife.com'));
+              filterSearchResults(searchResults, 'qwant', storage);
+            }
+
+            // Wait for document to be interactive/complete:
+            if (['interactive', 'complete'].includes(document.readyState)) {
+              filterQwant();
+            } else {
+              document.addEventListener('readystatechange', e => {
+                if (['interactive', 'complete'].includes(document.readyState)) {
+                  filterQwant();
                 }
               }, { once: true });
             }
