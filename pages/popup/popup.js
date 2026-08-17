7a6628db1c5754ca968bf5db96cac20f354ac5d1
Kevin Payravi <kevinpayravi@gmail.com>
2026-08-17 23:19:33 +0100

    Debounce popup

--- pages/popup/popup.js
+++ pages/popup/popup.js
@@ -2,6 +2,7 @@ import {
   extensionAPI,
   setDefaultUserAction,
  } from "../../scripts/common-functions.js";
+import { debounce } from "../common-page-functions.js";
 
 // Main function that runs on-load
 document.addEventListener('DOMContentLoaded', () => {
@@ -15,14 +16,17 @@ document.addEventListener('DOMContentLoaded', () => {
     window.close();
   });
 
+  // Debounced
+  const applyDefaultWikiAction = debounce(() => {
+    setDefaultUserAction('wikiSettings', document.options.defaultWikiAction.value);
+  }, 200);
   document.querySelectorAll('[name="defaultWikiAction"]').forEach((el) => {
-    el.addEventListener('change', () => {
-      setDefaultUserAction('wikiSettings', document.options.defaultWikiAction.value);
-    });
+    el.addEventListener('change', applyDefaultWikiAction);
   });
+  const applyDefaultSearchAction = debounce(() => {
+    setDefaultUserAction('searchEngineSettings', document.options.defaultSearchAction.value);
+  }, 200);
   document.querySelectorAll('[name="defaultSearchAction"]').forEach((el) => {
-    el.addEventListener('change', () => {
-      setDefaultUserAction('searchEngineSettings', document.options.defaultSearchAction.value);
-    });
+    el.addEventListener('change', applyDefaultSearchAction);
   });
 });
